0f463d31b903c6a025428f0ad331744dea144d2f
Zhengxu Chen <zhxchen17@fb.com>
2026-06-24 16:49:26 -0400

    Update build_jax.sh to include jaxlib in wheels

--- build_jax.sh
+++ build_jax.sh
@@ -1,1 +1,1 @@
-python build/build.py build --wheels=jax-cuda-plugin,jax-cuda-pjrt   --local_xla_path=../xla --bazel_options=--repo_env=LOCAL_CUDA_PATH="$(dirname $(dirname $(which nvcc)))"
+python build/build.py build --wheels=jaxlib,jax-cuda-plugin,jax-cuda-pjrt   --local_xla_path=../xla --bazel_options=--repo_env=LOCAL_CUDA_PATH="$(dirname $(dirname $(which nvcc)))"
