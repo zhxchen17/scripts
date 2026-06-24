ca60a16a7ef6ecd3ac735ca3d755b4a4c3f8be68
Zhengxu Chen <zhxchen17@fb.com>
2026-06-24 16:54:52 -0400

    Add pip install for jaxlib in build_jax.sh
    Added pip install command to install jaxlib from built wheels.

--- build_jax.sh
+++ build_jax.sh
@@ -1,1 +1,2 @@
 python build/build.py build --wheels=jaxlib,jax-cuda-plugin,jax-cuda-pjrt   --local_xla_path=../xla --bazel_options=--repo_env=LOCAL_CUDA_PATH="$(dirname $(dirname $(which nvcc)))"
+pip install dist/*.whl  # installs jaxlib (includes XLA)
